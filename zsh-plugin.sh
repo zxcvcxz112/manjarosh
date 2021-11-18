3edee6f80c003f7fae58bac8c64fa211626a8169
zxcvcxz112 <328440480@qq.com>
2021-11-19 03:57:38 +0800

    update

--- zsh-plugin.sh
+++ zsh-plugin.sh
@@ -1,5 +1,5 @@
 #dd
-https://github.com/robbyrussell/oh-my-zsh/archive/master.zip && unzip master.zip && cd ~/ohmyzsh-master/tools/ &&./install.sh
+git clone https://github.com/robbyrussell/oh-my-zsh/archive/master.zip && unzip master.zip && cd ~/ohmyzsh-master/tools/ &&./install.sh
 git clone git://github.com/wting/autojump.git && cd ./autojump/ && ./install.py
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
