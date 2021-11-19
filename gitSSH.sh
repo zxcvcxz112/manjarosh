a819cd9af6b64fe1530c434c83dd58a48ad85130
zxcvcxz112 <328440480@qq.com>
2021-11-19 22:47:42 +0800

    git

--- gitSSH.sh
+++ gitSSH.sh
@@ -1,4 +1,4 @@
-git config --golbal user.name "zxcvcxz112"
-git config --golbal user.email "328440480@qq.com"
-ssh-keygen -t rsa -C "328440480@qq.com"
+git config --golbal user.name ""
+git config --golbal user.email "@qq.com"
+ssh-keygen -t rsa -C ".com"
 cat ~/.ssh/id_rsa.pub
